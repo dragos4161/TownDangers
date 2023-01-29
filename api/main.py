9d3fe24c6b516efee76a526a6f51c437df54bd1f
dragos4161 <dima.dragos0@gmail.com>
2023-01-29 20:31:41 +0200

    Added the connection to Firestore and created a method to upload data

--- api/main.py
+++ api/main.py
@@ -1,7 +1,30 @@
 from fastapi import FastAPI
 
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+
 app = FastAPI()
+
 
 @app.get("/")
 async def root():
     return {'message': 'Everything works fine!'}
+
+@app.get("/add")
+async def add():
+    cred = credentials.Certificate("licenta-b9043-9411aff00f92.json")
+    firebase_admin.initialize_app(cred)
+    db = firestore.client()
+    ref = db.collection('users').document('dragos')
+    
+    ref.set({
+        'name':'Dragos',
+        'age':22
+    })
+    
+    return {'message': 'User Dragos, age 22 has been added in the database!'}
+
+
+
+
