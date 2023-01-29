f867f11b43bdbf6a0b13fe44fc5642e43e5a21d8
dragos4161 <dima.dragos0@gmail.com>
2023-01-29 20:08:01 +0200

    Implemented basic API to see if it works and created 2 classes

---
+++ api/Models/danger.py
@@ -0,0 +1,8 @@
+class Danger:
+    def __init__(self, uid, category, latitude, longitude, status) -> None:
+        self.uid = uid
+        self.category = category
+        self.latitude = latitude
+        self.longitude = longitude
+        self.status = status
+        
---
+++ api/Models/user.py
@@ -0,0 +1,8 @@
+from danger import Danger
+class User:
+    def __init__(self, name, uid, dangers, points) -> None:
+        self.name = name
+        self.uid = uid
+        self.dangers = dangers
+        self.pints = points
+        
